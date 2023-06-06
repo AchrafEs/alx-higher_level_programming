3ff85362ef4c19ebfcf8c934d5bdcc77b1252837
AchrafEs <achraf.es2110@gmail.com>
2023-06-06 23:16:24 +0000

    modified the function print_tebahpla again

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_alphabet():
-    for i in range(90, 64, -1):
-        print("{}{}".format(chr(i + 32), chr(i)), end="")
+    index = 0
+    for i in range(ord('z'), ord('a') - 1, -1):
+        print("{}".format(chr(i - i)), end="")
+        index = 32 if index == 0 else 0
