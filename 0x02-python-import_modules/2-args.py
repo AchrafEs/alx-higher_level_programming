647fc5d35153f7f8a007bcc6fcb0c9143be3f479
AchrafEs <achraf.es2110@gmail.com>
2023-06-09 15:53:16 +0000

    updated the file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from sys import argv
+
 if __name__ == "__main__":
     num_args = len(argv) - 1
-    print("{}: arguments".format(num_args), end="")
+    print("{} arguments".format(num_args), end="")
     if num_args == 0:
         print(".")
     else:
