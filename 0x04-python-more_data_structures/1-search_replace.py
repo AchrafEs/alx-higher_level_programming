69b955dc522f08eb74e80e334a823aa3516944ef
AchrafEs <achraf.es2110@gmail.com>
2023-06-14 14:20:13 +0000

    updated the function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     replaced_list = []
-    for row in my_list:
-        row_list =[]
-        for element in row:
-            if element == search:
-                row_list.append(replace)
-            else:
-                row_list.append(element)
-        replaced_list.append(row_list)
+    for element in my_list:
+        if element == search:
+            replaced_list.append(replace)
+        else:
+            replaced_list.append(element)
     return (replaced_list)
