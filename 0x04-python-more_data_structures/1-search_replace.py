2262bda9a20cd5576aea77f57a82c57f5ceb80db
AchrafEs <achraf.es2110@gmail.com>
2023-06-14 14:09:27 +0000

    added the file 1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    replaced_list = []
+    for row in my_list:
+        row_list = []
+        for element in row:
+            if element == search:
+                row_list.append(replace)
+            else:
+                row_list.append(element)
+        replaced_list.append(row_list)
+    return (replaced_list)
