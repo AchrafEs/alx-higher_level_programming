64124c74b719b1a1a20018f8fbef79dc0e5cba7f
AchrafEs <achraf.es2110@gmail.com>
2023-09-28 17:37:53 +0000

    added the script

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display all HTTP methods accepted by the server for a given URL
-curl -s -I -X OPTIONS "$1" | grep -i "Allow"
+curl -s -X OPTIONS -i "$1" | awk -F': ' '/^Allow/{print $2}' | tr -d '\r'
