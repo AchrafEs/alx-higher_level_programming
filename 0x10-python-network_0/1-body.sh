36e8442c9e09638bce771c208e888aeafd820d88
AchrafEs <achraf.es2110@gmail.com>
2023-09-28 17:25:23 +0000

    updated the script

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# a cript that takes in a URL, sends a GET request to the URL, and displays the body of the response
-(curl -s -I "$1" | grep -q "HTTP/1.1 200" && curl -s "$1") ; echo ""
+# a script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+(curl -s -I "$1" | grep -q " 200 " && curl -s "$1") ; echo ""
