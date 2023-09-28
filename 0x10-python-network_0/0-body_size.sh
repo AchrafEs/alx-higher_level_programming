d4e81bbce379afe1d03b60faa100027b68110dac
AchrafEs <achraf.es2110@gmail.com>
2023-09-28 16:42:56 +0000

    updated the script again

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # a script that takes in a URL and displays all HTTP methods the server will accept.
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <host:port>"
-  exit 1
-fi
-
 host_port="$1"
 
 body_size=$(curl -s "$host_port" | wc -c)
