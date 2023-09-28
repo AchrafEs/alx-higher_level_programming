ddb4257c06d5a808bae66828f47c12a086477c66
AchrafEs <achraf.es2110@gmail.com>
2023-09-28 16:34:28 +0000

    updated the script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
-# Check if a URL is provided as an argument
+# Check if a host:port is provided as an argument
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
+  echo "Usage: $0 <host:port>"
   exit 1
 fi
 
-url="$1"
+host_port="$1"
 
-# Use curl to fetch the URL and store the response in a temporary file
-response_file=$(mktemp)
-curl -s -o "$response_file" "$url"
-
-# Check if the request was successful (HTTP status 200 OK)
-http_status=$(curl -s -o /dev/null -w "%{http_code}" "$url")
-
-if [ "$http_status" -eq 200 ]; then
-  # Get the size of the response body in bytes
-  body_size=$(wc -c < "$response_file")
-  echo "Size of the response body: $body_size bytes"
-else
-  echo "Error: HTTP request failed with status code $http_status"
-fi
-
-# Clean up by removing the temporary response file
-rm "$response_file"
+# Use curl with -s to fetch the response and store the size of the response body in bytes
+body_size=$(curl -s "$host_port" | wc -c)
+echo "$body_size"
