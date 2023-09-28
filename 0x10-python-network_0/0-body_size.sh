dcb7a11c0208187e8d33b7b35e976456746fa146
AchrafEs <achraf.es2110@gmail.com>
2023-09-28 16:53:58 +0000

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # a script that takes in a URL and displays all HTTP methods the server will accept.
-
-host_port="$1"
-
-body_size=$(curl -s "$host_port" | wc -c)
-echo "$body_size"
+curl -s -I "$curl -s -I "http://0.0.0.0:5000" | awk -F': ' '/^Allow/{print $2}'
