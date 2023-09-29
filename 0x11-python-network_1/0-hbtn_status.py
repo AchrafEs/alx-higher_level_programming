d55570a84f830f9f5b3d4780614f4a4f5ccd15e3
AchrafEs <achraf.es2110@gmail.com>
2023-09-29 13:55:34 +0000

    added a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Fetches the url using urllib and displays response."""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(data)))
+            print("\t- content: {}".format(data.decode('utf-8')))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+    except urllib.error.URLError as e:
+        print("Error: {}".format(e.reason))
