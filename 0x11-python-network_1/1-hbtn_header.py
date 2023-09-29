2dc743d1476b4092fcc3193f292a0dec5242e8a3
AchrafEs <achraf.es2110@gmail.com>
2023-09-29 14:31:52 +0000

    fixed some issues

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,10 @@
 """
 Sends a request to a URL and displays the value of the
 X-Request-Id header variable.
+
+This script takes a URL as input, sends a request to that URL, and displays the
+value of the X-Request-Id header found in the response. It uses the urllib and
+sys packages for the HTTP request and command-line argument handling.
 """
 
 import urllib.request
