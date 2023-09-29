51ebe1997190b2562ce730fd670cafb29c8f6a8f
AchrafEs <achraf.es2110@gmail.com>
2023-09-29 14:18:36 +0000

    updated the script

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""Fetches the url using urllib and displays response."""
+
+"""
+Fetches the URL using urllib and displays response.
+
+This script uses the urllib package to fetch the status from the specified URL
+and displays information about the response, including its type and content.
+"""
 
 import urllib.request
 
@@ -17,3 +23,6 @@ if __name__ == "__main__":
         print("Error code: {}".format(e.code))
     except urllib.error.URLError as e:
         print("Error: {}".format(e.reason))
+
+if __name__ == "__main__":
+    print(__doc__)
