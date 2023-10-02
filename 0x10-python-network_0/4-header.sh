c3633526b0a4fd7e08d3fe31e8998251028d3194
AchrafEs <achraf.es2110@gmail.com>
2023-10-02 12:18:58 +0000

    updated the code

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send GET request to a URL with the X-School-User-Id header and display the response body
-curl -s -I -H "X-School-User-Id: 98" "$1" | awk -F': ' '/^X-School-User-Id/{print $2}' | tr -d '\r
+[ $# -ne 1 ] && echo "Usage: $0 <URL>" || curl -s -H "X-School-User-Id: 98" "$1"
