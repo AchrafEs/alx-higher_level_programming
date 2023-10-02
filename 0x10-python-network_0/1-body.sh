b9aea902c75aaa4081a59159c87eeed36cced09a
AchrafEs <achraf.es2110@gmail.com>
2023-10-02 12:27:19 +0000

    modified the script

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send GET request to a URL and display response body for HTTP 200 status code
-[ $# -ne 1 ] && echo "Usage: $0 <URL>" || { http_code=$(curl -s -o /dev/null -w "%{http_code}" "$1"); [ "$http_code" -eq 200 ] && curl -s "$1"; }
+[ $# -ne 1 ] && echo "Usage: $0 <URL>" || { redirections=0; url="$1"; while true; do response=$(curl -s -o /dev/null -w "%{http_code}" -L "$url"); echo "Status Code: $response"; if [ "$response" -eq 200 ]; then break; elif [ "$response" -ge 300 ] && [ "$response" -lt 400 ]; then url=$(curl -s -o /dev/null -w "%{redirect_url}" -L "$url"); echo "Redirecting to: $url"; ((redirections++)); else echo "Error: Unexpected status code $response"; break; fi; done; echo "Total Redirections: $redirections"; }
