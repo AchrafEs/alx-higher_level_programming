2a2303637e0837ab7bf7505aa8d3fc1df23a993b
AchrafEs <achraf.es2110@gmail.com>
2023-10-02 11:50:38 +0000

    updated

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a request to a URL and display only the status code
-[ $# -ne 1 ] && echo "Usage: $0 <URL>" || response=$(curl -s -o /dev/null -w "%{http_code}" "$1") && echo "Status Code: $response"
+curl -s -o /dev/null -w "%{http_code}" "$1"
